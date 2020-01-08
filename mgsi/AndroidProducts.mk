2cb5d89056bfef24ad4abc16e92ae06e1a723c27
Isaac Chen <ycchen@google.com>
2020-01-08 15:41:52 +0800

    Create bootable CSI products
    Bug: 147997588
    Test: $ lunch csi_x86-userdebug; m systemimg
          $ lunch csi_arm64-userdebug; m systemimg
          # Put a Pixel phone in fastboot state with AVB disabled
          $ fastboot flash system $OUT/system.img
          $ fastboot -w reboot
          $ adb root; adb shell
    
    Change-Id: I7abd4e2a7a9734e9b5bbda380f117f717cdd2eba

--- mgsi/AndroidProducts.mk
+++ mgsi/AndroidProducts.mk
@@ -17,5 +17,7 @@
 PRODUCT_MAKEFILES := \
 	device/generic/common/mgsi/mgsi_arm.mk \
 	device/generic/common/mgsi/mgsi_arm64.mk \
+	device/generic/common/mgsi/csi_arm64.mk \
 	device/generic/common/mgsi/mgsi_x86.mk \
+	device/generic/common/mgsi/csi_x86.mk \
 	device/generic/common/mgsi/mgsi_x86_64.mk \
